e09a592c887af5ed1a8121d32108f4bf9bfd7800
Nik <m3813155@gmail.com>
2024-12-25 12:39:26 +0500

    home

--- index.php
+++ index.php
@@ -9,6 +9,136 @@
 <body>
     <?php include "header.html" ?>
     
+    <section class="banner">
+        <img src="" alt="">
+    </section>
+    <section class="mission">
+        <h1></h1>
+        <div class="missionAll">
+            <div class="container">
+                <img src="" alt="">
+                <div class="text">
+                    <h1>lorem ipsum</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas nesciunt quis repudiandae rem minima quos necessitatibus ullam doloribus eligendi debitis, a quae itaque voluptatum aliquid?</p>
+                </div>
+            </div>
+            <div class="container">
+                <img src="" alt="">
+                <div class="text">
+                    <h1>lorem ipsum</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas nesciunt quis repudiandae rem minima quos necessitatibus ullam doloribus eligendi debitis, a quae itaque voluptatum aliquid?</p>
+                </div>
+            </div>
+            <div class="container">
+                <img src="" alt="">
+                <div class="text">
+                    <h1>lorem ipsum</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas nesciunt quis repudiandae rem minima quos necessitatibus ullam doloribus eligendi debitis, a quae itaque voluptatum aliquid?</p>
+                </div>
+            </div>
+            <div class="container">
+                <img src="" alt="">
+                <div class="text">
+                    <h1>lorem ipsum</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas nesciunt quis repudiandae rem minima quos necessitatibus ullam doloribus eligendi debitis, a quae itaque voluptatum aliquid?</p>
+                </div>
+            </div>
+            <div class="container">
+                <img src="" alt="">
+                <div class="text">
+                    <h1>lorem ipsum</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas nesciunt quis repudiandae rem minima quos necessitatibus ullam doloribus eligendi debitis, a quae itaque voluptatum aliquid?</p>
+                </div>
+            </div>
+            <div class="container">
+                <img src="" alt="">
+                <div class="text">
+                    <h1>lorem ipsum</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas nesciunt quis repudiandae rem minima quos necessitatibus ullam doloribus eligendi debitis, a quae itaque voluptatum aliquid?</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="about">
+
+        <img src="" alt="">
+
+        <div class="textAbout">
+
+            <h1>lorem ipsum</h1>
+            <h2>lorem ipsum</h2>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam natus placeat ipsam blanditiis quas error aliquid ipsa necessitatibus accusamus dolorem cupiditate iste recusandae animi, ut enim sequi, sed voluptatum consequuntur. <br>Explicabo assumenda, quis ut dolores perferendis eius laudantium molestias consequuntur temporibus at? Iure rem, molestias rerum, cupiditate qui quia veritatis atque voluptatem, labore aperiam soluta modi magni enim a? Repudiandae? Dolores odio laudantium quibusdam distinctio sequi corporis quo animi. Veniam enim, error porro earum tenetur molestiae nostrum a, sapiente quam repellat perspiciatis praesentium laborum nam ipsum laboriosam mollitia repudiandae necessitatibus.</p>
+        </div>
+        
+    </section>
+
+    <section class="review">
+        <h1>Lorem ipsum</h1>
+        <div class="reviewAll">
+            <div class="conyainerReview">
+                <h2>⚝⚝⚝⚝⚝</h2>
+                <h1>review</h1>
+                <p></p>
+                <div class="avatar">
+                    <img src="" alt="">
+                    <h1>name</h1>
+                    <p>date</p>
+                </div>
+            </div>
+            <div class="conyainerReview">
+                <h2>⚝⚝⚝⚝⚝</h2>
+                <h1>review</h1>
+                <p></p>
+                <div class="avatar">
+                    <img src="" alt="">
+                    <h1>name</h1>
+                    <p>date</p>
+                </div>
+            </div>
+            <div class="conyainerReview">
+                <h2>⚝⚝⚝⚝⚝</h2>
+                <h1>review</h1>
+                <p></p>
+                <div class="avatar">
+                    <img src="" alt="">
+                    <h1>name</h1>
+                    <p>date</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="social">
+            <img src="" alt="">
+            <div class="soc_net">
+                <img src="" alt="">
+                <img src="" alt=""><img src="" alt=""><img src="" alt="">
+            </div>
+        </div>
+        <div class="containerFooter">
+            <h1>lorem ipsum</h1>
+            <a href="">lorem</a>
+            <a href="">lorem</a>
+            <a href="">lorem</a>
+            <a href="">lorem</a></div>
+        <div class="containerFooter">
+            <h1>lorem ipsum</h1>
+            <a href="">lorem</a>
+            <a href="">lorem</a>
+            <a href="">lorem</a>
+            <a href="">lorem</a></div>
+        <div class="containerFooter">
+            <h1>lorem ipsum</h1>
+            <a href="">lorem</a>
+            <a href="">lorem</a>
+            <a href="">lorem</a>
+            <a href="">lorem</a></div>
+    </footer>
+    
+
     <script src="js/script.js"></script>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -9,26 +9,36 @@ const translations = {
     }
   };
   
-  // Текущий язык
+
   let currentLang = 'ru';
 
   document.getElementById('rus').addEventListener('click', function(){
     changeLanguage('ru');
     console.log(currentLang);
+    updatePageContent();
   })
 
   document.getElementById('eng').addEventListener('click', function(){
     changeLanguage('en');
     console.log(currentLang);
+    updatePageContent();
   })
   
-  // Функция смены языка
+
   function changeLanguage(lang) {
     currentLang = lang;
   }
   
-  // Функция получения перевода
+
   function translate(key) {
-    return translations[currentLang][key] || key; // Если перевода нет, вернуть оригинал
+    return translations[currentLang][key] || key; 
   }
-  
+
+
+  function updatePageContent() {
+   
+    document.getElementById('greeting').textContent = translate("Привет");
+    document.getElementById('status').textContent = translate("Как дела?");
+  }
+
+  updatePageContent();
